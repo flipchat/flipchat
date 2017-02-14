2f46eacc22cdbf428ee2f30b63f362566bda5998
Alex <jr.rus13@gmail.com>
2017-02-14 01:00:39 -0500

    Updates related to profs comments

--- SQL/00-schema.sql
+++ SQL/00-schema.sql
@@ -13,113 +13,113 @@ DROP TABLE IF EXISTS cs421g40.product;
 DROP TABLE IF EXISTS cs421g40.category;
 DROP TABLE IF EXISTS cs421g40.users;
 
-
 ---
 --- Table structure for FlipChat
 ---
 
 CREATE TABLE cs421g40.users (
-   email       VARCHAR(50)  NOT NULL,
-   password    VARCHAR(20)  NOT NULL,
-   full_name   VARCHAR(100) NOT NULL,
-   address     VARCHAR(100),
-   is_admin    BOOLEAN NOT NULL default FALSE,
-   admin_since TIMESTAMP NOT NULL,
-   PRIMARY KEY(email)
+  u_id        SERIAL       NOT NULL,
+  email       VARCHAR(50)  NOT NULL,
+  password    VARCHAR(20)  NOT NULL,
+  full_name   VARCHAR(100) NOT NULL,
+  address     VARCHAR(100),
+  is_admin    BOOLEAN      NOT NULL DEFAULT FALSE,
+  admin_since TIMESTAMP    NOT NULL,
+  PRIMARY KEY (u_id)
 );
 
 CREATE TABLE cs421g40.category (
-    cat_id        SERIAL NOT NULL,
-    name          VARCHAR(50) NOT NULL,
-    description   TEXT,
-    product_count INT NOT NULL default 0,
-    user_count    INT NOT NULL default 0,
-    email         VARCHAR(50) NOT NULL,
-    PRIMARY KEY (cat_id),
-    FOREIGN KEY (email) REFERENCES cs421g40.users(email)
+  cat_id        SERIAL      NOT NULL,
+  name          VARCHAR(50) NOT NULL,
+  description   TEXT,
+  product_count INT         NOT NULL DEFAULT 0,
+  user_count    INT         NOT NULL DEFAULT 0,
+  created_by    INT         NOT NULL,
+  PRIMARY KEY (cat_id),
+  FOREIGN KEY (created_by) REFERENCES cs421g40.users (u_id)
 );
 
 CREATE TABLE cs421g40.product (
-    p_id        SERIAL NOT NULL,
-    title       VARCHAR(50) NOT NULL,
-    description TEXT,
-    price       MONEY NOT NULL,
-    image       VARCHAR(50) NOT NULL,
-    is_sold     BOOLEAN NOT NULL default FALSE,
-    datetime    TIMESTAMP NOT NULL,
-    expiry      TIMESTAMP NOT NULL,
-    email       VARCHAR(50) NOT NULL,
-    cat_id      INT NOT NULL,
-    PRIMARY KEY(p_id),
-    FOREIGN KEY (email)  REFERENCES cs421g40.users(email),
-    FOREIGN KEY (cat_id) REFERENCES cs421g40.category(cat_id)
+  p_id        SERIAL      NOT NULL,
+  title       VARCHAR(50) NOT NULL,
+  description TEXT,
+  price       MONEY       NOT NULL,
+  image       VARCHAR(50) NOT NULL,
+  is_sold     BOOLEAN     NOT NULL DEFAULT FALSE,
+  datetime    TIMESTAMP   NOT NULL,
+  expiry      TIMESTAMP   NOT NULL,
+  u_id        INT         NOT NULL,
+  cat_id      INT         NOT NULL,
+  bid_id      INT,
+  PRIMARY KEY (p_id),
+  FOREIGN KEY (u_id) REFERENCES cs421g40.users (u_id),
+  FOREIGN KEY (cat_id) REFERENCES cs421g40.category (cat_id)
 );
 
 CREATE TABLE cs421g40.bid (
-    bid_id      SERIAL NOT NULL,
-    datetime    TIMESTAMP NOT NULL,
-    price       MONEY NOT NULL,
-    email       VARCHAR(50) NOT NULL,
-    p_id        INT NOT NULL,
-    t_id        INT NOT NULL,
-    PRIMARY KEY(bid_id),
-    FOREIGN KEY(email) REFERENCES cs421g40.users(email),
-    FOREIGN KEY(p_id)  REFERENCES cs421g40.product(p_id)
+  bid_id   SERIAL    NOT NULL,
+  datetime TIMESTAMP NOT NULL,
+  price    MONEY     NOT NULL,
+  u_id     INT       NOT NULL,
+  p_id     INT       NOT NULL,
+  t_id     INT       NOT NULL,
+  PRIMARY KEY (bid_id),
+  FOREIGN KEY (u_id) REFERENCES cs421g40.users (u_id),
+  FOREIGN KEY (p_id) REFERENCES cs421g40.product (p_id)
 );
 
 CREATE TABLE cs421g40.transaction (
-    t_id        SERIAL NOT NULL,
-    paid        BOOLEAN NOT NULL default FALSE,
-    datetime    TIMESTAMP NOT NULL,
-    email       VARCHAR(50) NOT NULL,
-    bid_id      INT NOT NULL,
-    PRIMARY KEY(t_id),
-    FOREIGN KEY(bid_id) REFERENCES cs421g40.bid(bid_id)
+  t_id     SERIAL    NOT NULL,
+  paid     BOOLEAN   NOT NULL DEFAULT FALSE,
+  datetime TIMESTAMP NOT NULL,
+  u_id     INT       NOT NULL,
+  bid_id   INT       NOT NULL,
+  PRIMARY KEY (t_id),
+  FOREIGN KEY (bid_id) REFERENCES cs421g40.bid (bid_id)
 );
 
-CREATE TABLE cs421g40.notification(
-    n_id      SERIAL NOT NULL,
-    content   TEXT NOT NULL,
-    type      INT,  -- 0-transaction, 1-new product, 2-wtv
-    email     VARCHAR(50) NOT NULL,
-    p_id      INT NOT NULL,
-    t_id      INT NOT NULL,
-    PRIMARY KEY(n_id),
-    FOREIGN KEY(email) REFERENCES cs421g40.users(email),
-    FOREIGN KEY(p_id)  REFERENCES cs421g40.product(p_id),
-    FOREIGN KEY(t_id)  REFERENCES cs421g40.transaction(t_id)
+-- TODO: Break down in multiple table to avoid data duplication
+CREATE TABLE cs421g40.notification (
+  n_id    SERIAL NOT NULL,
+  content TEXT   NOT NULL,
+  type    INT, -- 0-transaction, 1-new product, 2-wtv
+  p_id    INT,
+  t_id    INT,
+  PRIMARY KEY (n_id),
+  FOREIGN KEY (p_id) REFERENCES cs421g40.product (p_id),
+  FOREIGN KEY (t_id) REFERENCES cs421g40.transaction (t_id)
 );
 
-CREATE TABLE cs421g40.comment(
-    c_id        SERIAL NOT NULL,
-    content     TEXT NOT NULL,
-    datetime    TIMESTAMP NOT NULL,
-    email       VARCHAR(50) NOT NULL,
-    p_id        INT NOT NULL,
-    PRIMARY KEY(c_id),
-    FOREIGN KEY(email) REFERENCES cs421g40.users(email),
-    FOREIGN KEY(p_id)  REFERENCES cs421g40.product(p_id)
+CREATE TABLE cs421g40.comment (
+  c_id     SERIAL    NOT NULL,
+  content  TEXT      NOT NULL,
+  datetime TIMESTAMP NOT NULL,
+  u_id     INT       NOT NULL,
+  p_id     INT       NOT NULL,
+  PRIMARY KEY (c_id),
+  FOREIGN KEY (u_id) REFERENCES cs421g40.users (u_id),
+  FOREIGN KEY (p_id) REFERENCES cs421g40.product (p_id)
 );
 
-CREATE TABLE cs421g40.achievement(
-    a_id        SERIAL NOT NULL,
-    a_type      INT,  -- 0-seller, 1-buyer, 2-wtv
-    label       VARCHAR(50) NOT NULL,
-    condition   VARCHAR(50) NOT NULL,
-    description TEXT NOT NULL,
-    PRIMARY KEY(a_id)
+CREATE TABLE cs421g40.achievement (
+  a_id        SERIAL      NOT NULL,
+  a_type      INT, -- 0-seller, 1-buyer, 2-wtv
+  label       VARCHAR(50) NOT NULL,
+  condition   VARCHAR(50) NOT NULL,
+  description TEXT        NOT NULL,
+  PRIMARY KEY (a_id)
 );
 
-CREATE TABLE cs421g40.user_achievements(
-    a_id    INT NOT NULL,
-    email   VARCHAR(50) NOT NULL,
-    FOREIGN KEY(a_id)   REFERENCES cs421g40.achievement(a_id),
-    FOREIGN KEY(email)  REFERENCES cs421g40.users(email)
+CREATE TABLE cs421g40.user_achievements (
+  a_id INT NOT NULL,
+  u_id INT NOT NULL,
+  FOREIGN KEY (a_id) REFERENCES cs421g40.achievement (a_id),
+  FOREIGN KEY (u_id) REFERENCES cs421g40.users (u_id)
 );
 
-CREATE TABLE cs421g40.user_categories(
-    cat_id  INT NOT NULL,
-    email   VARCHAR(50) NOT NULL,
-    FOREIGN KEY(cat_id) REFERENCES cs421g40.category(cat_id),
-    FOREIGN KEY(email)  REFERENCES cs421g40.users(email)
+CREATE TABLE cs421g40.user_categories (
+  cat_id INT NOT NULL,
+  u_id   INT NOT NULL,
+  FOREIGN KEY (cat_id) REFERENCES cs421g40.category (cat_id),
+  FOREIGN KEY (u_id) REFERENCES cs421g40.users (u_id)
 );
