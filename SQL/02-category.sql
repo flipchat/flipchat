e8ca311f79c5e9ffd044c0493e1ba2b7823ec78c
Alex <jr.rus13@gmail.com>
2017-02-22 17:49:53 -0500

    Zains updates

--- SQL/02-category.sql
+++ SQL/02-category.sql
@@ -1,13 +1,13 @@
 
 -- Populate category table with a "real" data set of 10 records
 
-INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Electronics', 'Front-line transitional extranet', 76, 92, 75);
-INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Home', 'Proactive real-time productivity', 95, 89, 60);
-INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Books', 'Synchronised foreground strategy', 95, 88, 04);
-INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Clothing', 'Face to face regional attitude', 94, 82, 22);
-INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Sporting Goods', 'Digitized exuding contingency', 73, 86, 86);
-INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Toys', 'User-friendly maximized adapter', 99, 80, 77);
-INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Musical Instruments', 'Organic transitional firmware', 86, 94, 67);
-INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Cars', 'Fully-configurable zero tolerance local area network', 93, 85, 30);
-INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Accessories', 'Monitored human-resource portal', 86, 75, 14);
-INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Art', 'Secured reciprocal synergy', 93, 92, 73);
+INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Electronics', 'Front-line transitional extranet', 4, 0, 2);
+INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Home', 'Proactive real-time productivity', 8, 0, 87);
+INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Books', 'Synchronised foreground strategy', 12, 0, 97);
+INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Clothing', 'Face to face regional attitude', 14, 0, 42);
+INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Sporting Goods', 'Digitized exuding contingency', 8, 0, 15);
+INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Toys', 'User-friendly maximized adapter', 11, 0, 12);
+INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Musical Instruments', 'Organic transitional firmware', 11, 0, 12);
+INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Cars', 'Fully-configurable zero tolerance local area network', 9, 0, 100);
+INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Accessories', 'Monitored human-resource portal', 14, 0, 70);
+INSERT INTO category (name, description, product_count, user_count, created_by) VALUES ('Art', 'Secured reciprocal synergy', 9, 0, 25);
