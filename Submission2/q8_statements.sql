721e58f4c52db670188f85c3d18862418a0e77f5
Alex <jr.rus13@gmail.com>
2017-02-23 17:38:54 -0500

    Add failed inserts

--- Submission2/q8_statements.sql
+++ Submission2/q8_statements.sql
@@ -1,5 +1,16 @@
+--
+-- COMP 421 - Submission 02
+-- Question 08
+-- Group 40
+--
+
+-- Ensure that product prices are $0 or higher
 ALTER TABLE product
 ADD CONSTRAINT chk_Price CHECK (price::numeric >= 0);
 
+-- Ensure that comment datetime is less or equal than curent datetime
 ALTER TABLE comment
 ADD CONSTRAINT chk_Date CHECK (datetime <= now());
+
+
+-- Note test INSERT statements are inside the output file.
