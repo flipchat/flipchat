efde26e510b8fc84ba7d8ab219bc2bb52aa6789c
ZainV <drummerboyzain@rogers.com>
2017-02-23 14:47:39 -0500

    update

--- part3and4.sql
+++ part3and4.sql
@@ -1,6 +1,6 @@
 -- PART 3
 
-INSERT INTO bid (datetime, price, u_id, p_id, t_id) VALUES (CURRENT_TIMESTAMP, 75.00, 9, 99, null);
+INSERT INTO bid (datetime, price, u_id, p_id, t_id) VALUES (CURRENT_TIMESTAMP, 75.00, 9, 99, 5);
 INSERT INTO bid (datetime, price, u_id, p_id, t_id) VALUES (CURRENT_TIMESTAMP, 150.00, 69, 10, null);
 INSERT INTO bid (datetime, price, u_id, p_id, t_id) VALUES (CURRENT_TIMESTAMP, 2.00, 45, 27, null);
 INSERT INTO bid (datetime, price, u_id, p_id, t_id) VALUES (CURRENT_TIMESTAMP, 160.96, 93, 42, null);
