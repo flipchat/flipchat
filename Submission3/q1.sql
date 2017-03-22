6226511418a94c0e112077636598f59c35fd2af1
Kevin Tran <kevintran@asavpn234225.vpn.mcgill.ca>
2017-03-22 18:29:08 -0400

    q1 done

--- Submission3/q1.sql
+++ Submission3/q1.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE FUNCTION FIXBID (product_id INT)
 RETURNS void AS $what$
-   DECLARE
-    b_id INT;
-    b_price MONEY;
-    p_price MONEY;
-    at_end INT;
-    DECLARE C1 CURSOR FOR
-          SELECT bid_id, price FROM bid WHERE p_id = product_id;
+  DECLARE
+  b_id INT;
+  b_price MONEY;
+  p_price MONEY;
+  at_end INT;
+  DECLARE C1 CURSOR FOR
+    SELECT bid_id, price FROM bid WHERE p_id = product_id;
   BEGIN
         SELECT price INTO p_price FROM product where p_id = product_id;
         OPEN C1;
@@ -16,15 +16,15 @@ RETURNS void AS $what$
             IF at_end = 1 THEN RETURN;
             END IF;
             BEGIN
-            IF (b_price < p_price)
-              THEN UPDATE bid SET price = p_price WHERE bid_id = b_id;
-            END IF;
-            FETCH C1 INTO b_id, b_price;
-              EXCEPTION WHEN SQLSTATE '02000' THEN at_end := 1; RETURN;
+              IF (b_price < p_price)
+                THEN UPDATE bid SET price = p_price WHERE bid_id = b_id;
+              END IF;
+              FETCH C1 INTO b_id, b_price;
+              EXIT WHEN NOT FOUND;
             END;
         END LOOP;
         CLOSE C1;
-    END;
+  END;
   $what$ LANGUAGE plpgsql;
 
 SELECT FIXBID(29);
