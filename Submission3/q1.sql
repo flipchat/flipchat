59a23879fc7c525dd83806bde858476006f22f05
Kevin Tran <kevintran@Kevins-MacBook-Pro.local>
2017-03-22 18:11:49 -0400

    added q1

--- Submission3/q1.sql
+++ Submission3/q1.sql
@@ -1,22 +1,28 @@
-CREATE PROCEDURE FIXBID (IN product_id INT)
-    LANGUAGE SQL
-    BEGIN
-        DECLARE @p_price MONEY;
-        DECLARE at_end INT DEFAULT 0;
-        DECLARE not_found CONDITION FOR SQLSTATE '02000';
-        DECLARE C1 CURSOR FOR
-            SELECT bid_id, price FROM bid WHERE p_id = product_id;
-        DECLARE CONTINUE HANDLER FOR not_found SET at_end = 1;
-        SELECT @p_price = price FROM product where p_id = product_id;
-        DECLARE b_id INT;
-        DECLARE b_price MONEY;
+CREATE OR REPLACE FUNCTION FIXBID (product_id INT)
+RETURNS void AS $what$
+   DECLARE
+    b_id MONEY;
+    b_price MONEY;
+    p_price MONEY;
+    at_end INT;
+    DECLARE C1 CURSOR FOR
+          SELECT bid_id, price FROM bid WHERE p_id = product_id;
+  BEGIN
+        SELECT price INTO p_price FROM product where p_id = product_id;
         OPEN C1;
         FETCH C1 INTO b_id, b_price;
-        WHILE at_end = 0 DO
-            IF (b_price < @p_price)
-                THEN UPDATE bid SET price = @p_price WHERE bid_id = b_id;
+        at_end := 0;
+        LOOP
+            IF at_end = 1 THEN RETURN;
+            END IF;
+            BEGIN
+            IF (b_price < p_price)
+                THEN UPDATE bid SET price = p_price WHERE bid_id = b_id;
             END IF;
             FETCH C1 INTO b_id, b_price;
-        END WHILE;
+            EXCEPTION WHEN SQLSTATE '02000' THEN at_end := 1;
+            END;
+        END LOOP;
         CLOSE C1;
-    END
+    END;
+  $what$ LANGUAGE plpgsql;
